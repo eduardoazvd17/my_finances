683328a331e542a32087e51d3bc3baa922b34e6a
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-02 11:04:29 -0300

    Creating a def button widget

---
+++ lib/src/core/presentation/widgets/button_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final void Function() onTap;
+  final double borderRadius;
+  final Icon? icon;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final Color? borderColor;
+
+  const ButtonWidget({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.borderRadius = 10,
+    this.icon,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.borderColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      highlightColor: Colors.grey.withOpacity(0.25),
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          border: Border.all(
+            color: borderColor ?? Theme.of(context).primaryColor,
+          ),
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (icon != null)
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: icon,
+                ),
+              Text(
+                text,
+                style: TextStyle(
+                  color: foregroundColor ?? Theme.of(context).primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
