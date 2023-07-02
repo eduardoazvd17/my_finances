61cb940c63bb58f0baa7ab773240caf2e74e4d32
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-02 11:04:48 -0300

    Finishing welcome screen UI

--- lib/src/features/authentication/presentation/pages/welcome_page.dart
+++ lib/src/features/authentication/presentation/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
 import 'package:myfinances/src/core/presentation/widgets/app_logo.dart';
+import 'package:myfinances/src/core/presentation/widgets/button_widget.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -11,66 +12,62 @@ class WelcomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const AppLogo(
-                verticalAlign: true,
-                size: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Text(
-                  'welcome-text'.i18n(),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 50),
-                child: Text(
-                  'slogan-text'.i18n(),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.grey),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            Theme.of(context).primaryColor,
-                          ),
-                          foregroundColor: MaterialStateProperty.all(
-                            Colors.white,
-                          ),
-                          overlayColor: MaterialStateProperty.all(
-                            Colors.grey.withOpacity(0.25),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const AppLogo(
+                    verticalAlign: true,
+                    size: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Text(
+                      'welcome-text'.i18n(),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 80),
+                    child: Text(
+                      'slogan-text'.i18n(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ButtonWidget(
+                            text: 'login-button-text'.i18n(),
+                            backgroundColor: Theme.of(context).primaryColor,
+                            foregroundColor: Colors.white,
+                            onTap: () {},
                           ),
                         ),
-                        child: Text('login-button-text'.i18n()),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0, right: 30, left: 30),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text('register-button-text'.i18n()),
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ButtonWidget(
+                            text: 'register-button-text'.i18n(),
+                            onTap: () {},
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
