6f8a9b84463ddf20f9d97a9194b20b5423b7f6d0
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-03 11:29:52 -0300

    Removing imports

--- lib/src/features/authentication/presentation/controllers/auth_controller.dart
+++ lib/src/features/authentication/presentation/controllers/auth_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:localization/localization.dart';
 import 'package:myfinances/src/core/data/errors/app_error.dart';
