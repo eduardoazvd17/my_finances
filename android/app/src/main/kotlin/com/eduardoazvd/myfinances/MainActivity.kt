ca28d061d9bc4a64173ee2b9f944dd1aa8af99f7
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-03 23:34:53 -0300

    Fixing android build errors

--- android/app/src/main/kotlin/com/eduardoazvd/myfinances/MainActivity.kt
+++ android/app/src/main/kotlin/com/eduardoazvd/myfinances/MainActivity.kt
@@ -1,6 +1,4 @@
-package com.eduardoazvd.myfinances
-
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
 }
