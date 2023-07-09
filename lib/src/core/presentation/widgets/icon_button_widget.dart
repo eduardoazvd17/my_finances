5fb324bdc5e9562a84e597d6353e9e2b00a3c8fd
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-09 12:48:54 -0300

    Adjusting icon button

--- lib/src/core/presentation/widgets/icon_button_widget.dart
+++ lib/src/core/presentation/widgets/icon_button_widget.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
 
 class IconButtonWidget extends StatelessWidget {
+  final String tooltip;
   final IconData icon;
   final void Function() onTap;
-  final String tooltip;
-  final bool compactMode;
+  final Color? iconColor;
+  final Color? backgroundColor;
+  final double borderRadius;
   const IconButtonWidget({
     super.key,
+    required this.tooltip,
     required this.icon,
     required this.onTap,
-    required this.tooltip,
-    this.compactMode = false,
+    this.iconColor,
+    this.backgroundColor,
+    this.borderRadius = 10,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: tooltip,
-      child: InkWell(
-        onTap: onTap,
-        borderRadius: BorderRadius.circular(50),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
+    return Material(
+      borderRadius: BorderRadius.circular(borderRadius),
+      color: backgroundColor ?? Theme.of(context).primaryColor,
+      child: Tooltip(
+        message: tooltip,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(borderRadius),
           child: Padding(
             padding: const EdgeInsets.all(5.0),
             child: Icon(
               icon,
-              color: Theme.of(context).primaryColor,
+              color: iconColor ?? Colors.white,
             ),
           ),
         ),
