6caacef184fe960a308cbc982efcbd368d80ee7d
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-13 15:49:31 -0300

    Increasing width for web

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MyFinancesApp extends GetWidget<I18nController> {
               ),
             );
           },
-          maximumSize: Size(500, Get.height),
+          maximumSize: Size(768, Get.height),
           enabled: kIsWeb,
           backgroundColor:
               themeController.selectedTheme.webFrameBackgroundColor,
