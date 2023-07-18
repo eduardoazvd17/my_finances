a340323bccc26392b4742ca6b314cb6aa00cc6bc
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-07-18 15:57:29 -0300

    Adding bottom menu option

--- lib/src/core/presentation/widgets/scaffold_widget.dart
+++ lib/src/core/presentation/widgets/scaffold_widget.dart
@@ -4,11 +4,13 @@ class ScaffoldWidget extends StatelessWidget {
   final PreferredSizeWidget? appBar;
   final Widget? body;
   final Widget? floatingBottomMenu;
+  final Widget? bottomNavigationMenu;
   const ScaffoldWidget({
     super.key,
     this.appBar,
     this.body,
     this.floatingBottomMenu,
+    this.bottomNavigationMenu,
   });
 
   void _hideKeyboard() => FocusManager.instance.primaryFocus?.unfocus();
@@ -21,6 +23,7 @@ class ScaffoldWidget extends StatelessWidget {
         appBar: appBar,
         floatingActionButton: floatingBottomMenu,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        bottomNavigationBar: bottomNavigationMenu,
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
