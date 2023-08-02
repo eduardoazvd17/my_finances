d2a26eb189c98c563e8ba8d798adbd949d5ae8f7
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-02 13:46:12 -0300

    Fixing date time utils

--- lib/src/core/data/utils/date_time_utils.dart
+++ lib/src/core/data/utils/date_time_utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:myfinances/src/core/presentation/controllers/i18n_controller.dart';
@@ -17,25 +18,37 @@ class DateTimeUtils {
   }
 
   static String formatFullDateShorted(DateTime date) {
-    final time =
-        '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
-    final weekDay = DateFormat.E(_localeString).format(date);
+    final String time = _time(date);
+    final String weekDay = _weekDay(date, shortted: true);
     return '$weekDay, ${DateFormat.yMd(_localeString).format(date)} $time';
   }
 
   static String formatFullDate(DateTime date) {
-    final time =
-        '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
-    final weekDay = DateFormat.EEEE(_localeString).format(date);
+    final String time = _time(date);
+    final String weekDay = _weekDay(date);
     return '$weekDay, ${DateFormat.yMMMMd(_localeString).format(date)} - $time';
   }
 
   static String formatDate(DateTime date) {
-    final weekDay = DateFormat.EEEE(_localeString).format(date);
+    final weekDay = _weekDay(date);
     return '$weekDay, ${DateFormat.yMd(_localeString).format(date)}';
   }
 
   static String formatShortDate(DateTime date) {
     return DateFormat.yMd(_localeString).format(date);
+  }
+
+  static String _time(DateTime date) {
+    return '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+  }
+
+  static String _weekDay(DateTime date, {bool shortted = false}) {
+    final weekDay = shortted
+        ? DateFormat.E(_localeString).format(date)
+        : DateFormat.EEEE(_localeString).format(date);
+    return weekDay.replaceFirst(
+      weekDay.characters.first,
+      weekDay.characters.first.toUpperCase(),
+    );
   }
 }
