1bee2566c0129a8eca853371e77cb59eae874049
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-08 11:35:38 -0300

    Adding close button to all modals

--- lib/src/core/presentation/widgets/bottom_sheet_modal_widget.dart
+++ lib/src/core/presentation/widgets/bottom_sheet_modal_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:localization/localization.dart';
 import 'package:myfinances/src/core/presentation/widgets/scroll_view_widget.dart';
 
 class BottomSheetModalWidget extends StatelessWidget {
@@ -14,59 +16,75 @@ class BottomSheetModalWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Container(
-                  height: 8,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.grey[600],
-                  ),
-                ),
-              ),
-              if (title != null)
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (icon != null)
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Icon(icon),
-                        ),
-                      Text(
-                        title!,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+    return Stack(
+      children: [
+        SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 5,
+                      top: 19.5,
+                    ),
+                    child: Container(
+                      height: 8,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey[600],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              Flexible(
-                child: ScrollViewWidget(
-                  showBar: true,
-                  child: child,
-                ),
+                  if (title != null)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (icon != null)
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0),
+                              child: Icon(icon),
+                            ),
+                          Text(
+                            title!,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  Flexible(
+                    child: ScrollViewWidget(
+                      showBar: true,
+                      child: child,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: IconButton(
+            onPressed: Get.back,
+            tooltip: 'close-button'.i18n(),
+            icon: const Icon(Icons.close, color: Colors.red),
+          ),
+        ),
+      ],
     );
   }
 }
