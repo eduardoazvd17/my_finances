867be597718a92212d944904fbec458aee11385d
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-11 17:43:23 -0300

    Adding my profile page contents

--- lib/src/features/my_profile/presentation/controllers/my_profile_controller.dart
+++ lib/src/features/my_profile/presentation/controllers/my_profile_controller.dart
@@ -1,3 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class MyProfileController extends GetxController {}
+import '../../../../core/presentation/controllers/app_controller.dart';
+
+class MyProfileController extends GetxController {
+  late final RxString _name;
+  late final RxString _email;
+  late final RxString _passwordHash;
+
+  @override
+  void onInit() {
+    _name = RxString(AppController.instance.user!.name);
+    _email = RxString(AppController.instance.user!.email);
+    _passwordHash = RxString(AppController.instance.user!.password);
+    super.onInit();
+  }
+
+  String get name => _name.value;
+  String get email => _email.value;
+  String get passwordHash => _passwordHash.value;
+
+  final nameController = TextEditingController();
+  final nameFocus = FocusNode();
+
+  final oldPasswordController = TextEditingController();
+  final oldPasswordFocus = FocusNode();
+
+  final passwordController = TextEditingController();
+  final passwordFocus = FocusNode();
+
+  final password2Controller = TextEditingController();
+  final password2Focus = FocusNode();
+
+  Future<bool> editName() async {
+    return true;
+  }
+
+  Future<bool> editPassword() async {
+    return true;
+  }
+}
--- lib/src/features/my_profile/presentation/pages/my_profile_page.dart
+++ lib/src/features/my_profile/presentation/pages/my_profile_page.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:localization/localization.dart';
+
 import 'package:myfinances/src/core/presentation/widgets/scaffold_widget.dart';
+import 'package:myfinances/src/core/presentation/widgets/scroll_view_widget.dart';
+
 import 'package:myfinances/src/features/my_profile/presentation/controllers/my_profile_controller.dart';
 
 class MyProfilePage extends GetWidget<MyProfileController> {
@@ -10,10 +14,83 @@ class MyProfilePage extends GetWidget<MyProfileController> {
   @override
   Widget build(BuildContext context) {
     return ScaffoldWidget(
-      appBar: AppBar(
-        title: Text('my-profile-button'.i18n()),
+      appBar: AppBar(title: Text('my-profile-button'.i18n())),
+      body: ScrollViewWidget(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: ClipOval(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Theme.of(context).primaryColor,
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(CupertinoIcons.person,
+                          size: 50, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Obx(
+              () => itemTile(
+                label: 'name-text'.i18n(),
+                content: controller.name,
+              ),
+            ),
+            itemTile(
+              label: 'email-text'.i18n(),
+              content: controller.email,
+            ),
+            itemTile(
+              label: 'password-text'.i18n(),
+              content: '********',
+            ),
+          ],
+        ),
       ),
-      body: Container(),
+    );
+  }
+
+  Widget itemTile({
+    required String label,
+    required String content,
+    void Function()? onEdit,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label, style: TextStyle(color: Colors.grey[600])),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 8.0,
+              vertical: onEdit != null ? 0 : 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: Text(content)),
+                    if (onEdit != null)
+                      IconButton(
+                        onPressed: onEdit,
+                        tooltip: 'edit-text'.i18n(),
+                        icon: const Icon(CupertinoIcons.pen),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Divider(),
+        ],
+      ),
     );
   }
 }
