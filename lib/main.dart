d94adc12060090bd32eb51e5e729a4bf434af72f
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-17 17:23:31 -0300

    Fix getx error

--- lib/main.dart
+++ lib/main.dart
@@ -43,33 +43,30 @@ class MyFinancesApp extends GetWidget<I18nController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      final themeController = Get.find<ThemeController>();
-      return FlutterWebFrame(
-        builder: (context) {
-          return Obx(
-            () => GetMaterialApp(
-              builder: (_, child) => LifeCycleHandler(child: child!),
-              debugShowCheckedModeBanner: false,
-              title: controller.appName,
-              onGenerateTitle: (_) => controller.appName,
-              locale: controller.selectedLocale,
-              localizationsDelegates: controller.localizationsDelegates,
-              supportedLocales: controller.supportedLocales,
-              localeResolutionCallback: controller.localeResolutionCallback,
-              theme: AppThemes.light,
-              darkTheme: AppThemes.dark,
-              themeMode: themeController.selectedTheme.themeMode,
-              initialBinding: AppBinding(),
-              initialRoute: AppRoutes.initialRoute,
-              getPages: AppRoutes.getGetPages(),
-            ),
-          );
-        },
-        maximumSize: Size(800, Get.height),
-        enabled: kIsWeb,
-        backgroundColor: AppThemes.commonColor,
-      );
-    });
+    return FlutterWebFrame(
+      builder: (context) {
+        return Obx(
+          () => GetMaterialApp(
+            builder: (_, child) => LifeCycleHandler(child: child!),
+            debugShowCheckedModeBanner: false,
+            title: controller.appName,
+            onGenerateTitle: (_) => controller.appName,
+            locale: controller.selectedLocale,
+            localizationsDelegates: controller.localizationsDelegates,
+            supportedLocales: controller.supportedLocales,
+            localeResolutionCallback: controller.localeResolutionCallback,
+            theme: AppThemes.light,
+            darkTheme: AppThemes.dark,
+            themeMode: Get.find<ThemeController>().selectedTheme.themeMode,
+            initialBinding: AppBinding(),
+            initialRoute: AppRoutes.initialRoute,
+            getPages: AppRoutes.getGetPages(),
+          ),
+        );
+      },
+      maximumSize: Size(800, Get.height),
+      enabled: kIsWeb,
+      backgroundColor: AppThemes.commonColor,
+    );
   }
 }
