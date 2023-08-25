7ec575fa39781dd677f8062734643fdca48108c2
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-25 15:34:08 -0300

    Improving profile picture page

--- lib/src/features/my_profile/presentation/pages/my_profile_picture_page.dart
+++ lib/src/features/my_profile/presentation/pages/my_profile_picture_page.dart
@@ -5,61 +5,113 @@ import 'package:myfinances/src/core/data/errors/app_error.dart';
 
 import '../../../../core/data/utils/app_themes.dart';
 
-class MyProfilePicturePage extends StatelessWidget {
+class MyProfilePicturePage extends StatefulWidget {
   const MyProfilePicturePage({super.key});
 
   @override
+  State<MyProfilePicturePage> createState() => _MyProfilePicturePageState();
+}
+
+class _MyProfilePicturePageState extends State<MyProfilePicturePage> {
+  bool _hideOptions = false;
+  bool _disableOptionsUsage = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
         backgroundColor: Colors.black,
-        leading: IconButton(
-          onPressed: Get.back,
-          icon: const Icon(
-            Icons.close,
-            color: Colors.white,
-          ),
-        ),
-        title: Text(
-          'profile-picture-text'.i18n(),
-          style: const TextStyle(color: Colors.white),
+        body: Stack(
+          children: [
+            _interactiveViewerWidget,
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: AnimatedOpacity(
+                opacity: _hideOptions ? 0 : 1,
+                duration: const Duration(milliseconds: 100),
+                onEnd: () {
+                  setState(() {
+                    _disableOptionsUsage = _hideOptions;
+                  });
+                },
+                child: _appBar,
+              ),
+            ),
+          ],
         ),
       ),
-      backgroundColor: Colors.black,
-      body: Row(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                InteractiveViewer(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
+    );
+  }
+
+  Widget get _appBar {
+    return GestureDetector(
+      onTap: () {
+        if (_hideOptions) {
+          setState(() {
+            _hideOptions = false;
+          });
+        }
+      },
+      child: AppBar(
+        backgroundColor: Colors.black54,
+        foregroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: _disableOptionsUsage ? null : Get.back,
+          icon: const Icon(Icons.close),
+        ),
+        title: Text('profile-picture-text'.i18n()),
+      ),
+    );
+  }
+
+  Widget get _interactiveViewerWidget {
+    return InteractiveViewer(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
                     child: DecoratedBox(
                       decoration: BoxDecoration(
                         border: Border.all(color: AppThemes.commonColor),
                       ),
                       child: Padding(
                         padding: const EdgeInsets.all(1.0),
-                        child: Image.network(
-                          Get.arguments?.toString() ?? '',
-                          errorBuilder: (_, __, ___) {
-                            return Text(
-                              AppError.generic().message,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(color: Colors.white),
-                            );
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _hideOptions = !_hideOptions;
+                            });
                           },
+                          child: Image.network(
+                            Get.arguments?.toString() ?? '',
+                            errorBuilder: (_, __, ___) {
+                              return Text(
+                                AppError.generic().message,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(color: Colors.white),
+                              );
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
