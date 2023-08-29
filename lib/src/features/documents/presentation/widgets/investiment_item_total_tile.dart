619aa5b374fbd552c7d84246f97751d6b039cbcd
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-08-29 14:39:19 -0300

    Improving total tile for investiments

--- lib/src/features/documents/presentation/widgets/investiment_item_total_tile.dart
+++ lib/src/features/documents/presentation/widgets/investiment_item_total_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 
+import '../../../../core/data/utils/app_themes.dart';
 import '../../../../core/data/utils/currency_utils.dart';
 import '../../data/enums/operation_type.dart';
 
@@ -25,40 +27,89 @@ class InvestimentItemTotalTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 5),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Text(
-                  title,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Text('$quotas ${CurrencyUtils.format(quotasValue)}'),
-            ],
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  '${OperationType.purchase.char} $purchasesQuotas ${CurrencyUtils.format(purchasesValue)}',
-                  style: const TextStyle(color: Colors.green),
-                ),
-              ),
-              Flexible(
-                child: Text(
-                  '${OperationType.sell.char} $salesQuotas ${CurrencyUtils.format(salesValue)}',
-                  style: TextStyle(color: Colors.red[300]),
-                ),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: _tableWidget(),
           ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+
+  Widget _tableWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Flexible(
+          child: _getTableColumn(
+            alignment: CrossAxisAlignment.center,
+            label: 'Operation',
+            first: OperationType.purchase.icon,
+            second: OperationType.sell.icon,
+            third: Text(
+              'current-text'.i18n(),
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Flexible(
+          child: _getTableColumn(
+            alignment: CrossAxisAlignment.center,
+            label: 'Quotas',
+            first: Text(
+              '$purchasesQuotas',
+              style: const TextStyle(color: Colors.green),
+            ),
+            second: Text(
+              '$salesQuotas',
+              style: TextStyle(color: Colors.red[300]),
+            ),
+            third: Text('$quotas'),
+          ),
+        ),
+        Flexible(
+          child: _getTableColumn(
+            alignment: CrossAxisAlignment.end,
+            label: 'Value',
+            first: Text(
+              CurrencyUtils.format(purchasesValue),
+              style: const TextStyle(color: Colors.green),
+            ),
+            second: Text(
+              CurrencyUtils.format(salesValue),
+              style: TextStyle(color: Colors.red[300]),
+            ),
+            third: Text(CurrencyUtils.format(quotasValue)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getTableColumn({
+    required String label,
+    required CrossAxisAlignment alignment,
+    required Widget first,
+    required Widget second,
+    required Widget third,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        crossAxisAlignment: alignment,
+        children: [
+          Center(child: FittedBox(child: Text(label))),
+          FittedBox(child: first),
+          FittedBox(child: second),
+          FittedBox(child: third),
         ],
       ),
     );
