c9bbbf68c160fa82b3f41efe0afc76ffa6e40cd9
Eduardo Azevedo <eduardoazvd17@gmail.com>
2023-09-12 17:11:03 -0300

    Limiting login and register page at 500px width

--- lib/src/features/authentication/presentation/pages/register_page.dart
+++ lib/src/features/authentication/presentation/pages/register_page.dart
@@ -29,93 +29,97 @@ class RegisterPage extends GetWidget<AuthController> {
         title: Text('register-button'.i18n()),
       ),
       body: Center(
-        child: ScrollViewWidget(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 50.0, top: 16),
-                child: AppLogo(),
-              ),
-              AutofillGroup(
-                child: Column(
-                  children: [
-                    TextFieldWidget(
-                      autofillHints: const [AutofillHints.name],
-                      icon: CupertinoIcons.person,
-                      label: 'register-name-label'.i18n(),
-                      hint: 'register-name-hint'.i18n(),
-                      controller: controller.nameController,
-                      textInputType: TextInputType.text,
-                      textCapitalization: TextCapitalization.words,
-                      focusNode: controller.nameFocus,
-                      textInputAction: TextInputAction.next,
-                      onSubmitted: (_) => controller.emailFocus.requestFocus(),
-                    ),
-                    TextFieldWidget(
-                      autofillHints: const [AutofillHints.email],
-                      icon: CupertinoIcons.mail,
-                      label: 'register-email-label'.i18n(),
-                      hint: 'register-email-hint'.i18n(),
-                      controller: controller.emailController,
-                      textInputType: TextInputType.emailAddress,
-                      focusNode: controller.emailFocus,
-                      textInputAction: TextInputAction.next,
-                      onSubmitted: (_) =>
-                          controller.passwordFocus.requestFocus(),
-                    ),
-                    TextFieldWidget(
-                      autofillHints: const [AutofillHints.password],
-                      icon: CupertinoIcons.lock,
-                      label: 'register-password-label'.i18n(),
-                      hint: 'register-password-hint'.i18n(),
-                      controller: controller.passwordController,
-                      textInputType: TextInputType.text,
-                      obscureText: true,
-                      focusNode: controller.passwordFocus,
-                      textInputAction: TextInputAction.next,
-                      onSubmitted: (_) =>
-                          controller.password2Focus.requestFocus(),
-                    ),
-                    TextFieldWidget(
-                      icon: CupertinoIcons.lock_shield,
-                      label: 'register-password2-label'.i18n(),
-                      hint: 'register-password2-hint'.i18n(),
-                      controller: controller.password2Controller,
-                      textInputType: TextInputType.text,
-                      obscureText: true,
-                      focusNode: controller.password2Focus,
-                      textInputAction: TextInputAction.done,
-                      onSubmitted: (_) {
-                        TextInput.finishAutofillContext();
-                        controller.makeRegister();
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 16,
-                        bottom: 16,
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: ButtonWidget(
-                              icon: CupertinoIcons.person_add,
-                              text: 'make-register-button'.i18n(),
-                              backgroundColor: Theme.of(context).primaryColor,
-                              foregroundColor: Colors.white,
-                              onTap: () {
-                                TextInput.finishAutofillContext();
-                                controller.makeRegister();
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: ScrollViewWidget(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0, top: 16),
+                  child: AppLogo(),
                 ),
-              ),
-            ],
+                AutofillGroup(
+                  child: Column(
+                    children: [
+                      TextFieldWidget(
+                        autofillHints: const [AutofillHints.name],
+                        icon: CupertinoIcons.person,
+                        label: 'register-name-label'.i18n(),
+                        hint: 'register-name-hint'.i18n(),
+                        controller: controller.nameController,
+                        textInputType: TextInputType.text,
+                        textCapitalization: TextCapitalization.words,
+                        focusNode: controller.nameFocus,
+                        textInputAction: TextInputAction.next,
+                        onSubmitted: (_) =>
+                            controller.emailFocus.requestFocus(),
+                      ),
+                      TextFieldWidget(
+                        autofillHints: const [AutofillHints.email],
+                        icon: CupertinoIcons.mail,
+                        label: 'register-email-label'.i18n(),
+                        hint: 'register-email-hint'.i18n(),
+                        controller: controller.emailController,
+                        textInputType: TextInputType.emailAddress,
+                        focusNode: controller.emailFocus,
+                        textInputAction: TextInputAction.next,
+                        onSubmitted: (_) =>
+                            controller.passwordFocus.requestFocus(),
+                      ),
+                      TextFieldWidget(
+                        autofillHints: const [AutofillHints.password],
+                        icon: CupertinoIcons.lock,
+                        label: 'register-password-label'.i18n(),
+                        hint: 'register-password-hint'.i18n(),
+                        controller: controller.passwordController,
+                        textInputType: TextInputType.text,
+                        obscureText: true,
+                        focusNode: controller.passwordFocus,
+                        textInputAction: TextInputAction.next,
+                        onSubmitted: (_) =>
+                            controller.password2Focus.requestFocus(),
+                      ),
+                      TextFieldWidget(
+                        icon: CupertinoIcons.lock_shield,
+                        label: 'register-password2-label'.i18n(),
+                        hint: 'register-password2-hint'.i18n(),
+                        controller: controller.password2Controller,
+                        textInputType: TextInputType.text,
+                        obscureText: true,
+                        focusNode: controller.password2Focus,
+                        textInputAction: TextInputAction.done,
+                        onSubmitted: (_) {
+                          TextInput.finishAutofillContext();
+                          controller.makeRegister();
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 16,
+                          bottom: 16,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: ButtonWidget(
+                                icon: CupertinoIcons.person_add,
+                                text: 'make-register-button'.i18n(),
+                                backgroundColor: Theme.of(context).primaryColor,
+                                foregroundColor: Colors.white,
+                                onTap: () {
+                                  TextInput.finishAutofillContext();
+                                  controller.makeRegister();
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
